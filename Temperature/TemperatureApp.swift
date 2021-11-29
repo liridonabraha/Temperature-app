ce46bc5041d9a46159057606d2fdf5f54c84a33b
Dom Bryan <dombguitar@gmail.com>
2021-11-29 13:29:32 +0000

    Initial Commit

---
+++ Temperature/TemperatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureApp.swift
+//  Temperature
+//
+//  Created by Dom Bryan on 29/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
